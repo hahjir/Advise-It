c34eeda42289a81165d60e9308e94fd6b4d91a5b
hahjir <91931469+hahjir@users.noreply.github.com>
2023-01-16 16:16:28 -0800

    Attempted PBI #3

--- planner.php
+++ planner.php
@@ -1,4 +1,29 @@
 <?php
+//// Program to display URL of current page.
+//if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+//    $link = "https";
+//else $link = "http";
+//
+//// Here append the common URL characters.
+//$link .= "://";
+//
+//// Append the host(domain name, ip) to the URL.
+//$link .= $_SERVER['HTTP_HOST'];
+//
+//// Append the requested resource location to the URL
+//$link .= $_SERVER['REQUEST_URI'];
+//
+//// Print the link
+//echo $link . "<br>";
+//
+//
+//$id = substr($link, strrpos($link, '/') + 1);
+//
+//echo $id;
+//?>
+
+
+<?php
 session_start();
 if (empty ($_POST)) {
     $token = bin2hex(random_bytes(3));
