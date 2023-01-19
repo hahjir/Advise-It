e956ff27436863b794b0b04073962abc2193e1d3
hahjir <91931469+hahjir@users.noreply.github.com>
2023-01-19 15:53:52 -0800

    Added advisor column to database, completed PBI #4

--- planner.php
+++ planner.php
@@ -8,7 +8,7 @@ or die("error connecting");
 $validToken = $_GET["token"];
 
 
-if ( !is_null($validToken)) {
+if (!is_null($validToken)) {
     $_SESSION["token"] = $validToken;
 
     $query = "SELECT * FROM `adviseIt` WHERE token = '$validToken'";
@@ -17,11 +17,12 @@ if ( !is_null($validToken)) {
     if (empty(mysqli_fetch_row($result))) {
         header("Location: https://halsamach.greenriverdev.com/home.html");
     }
-    foreach ($result as $row){
+    foreach ($result as $row) {
         $newFall = $row["fall"];
         $newWinter = $row["winter"];
-        $newSpring= $row["spring"];
+        $newSpring = $row["spring"];
         $newSummer = $row["summer"];
+        $newAdvisor = $row["advisor"];
 
     }
 
@@ -33,8 +34,6 @@ if (empty ($_POST) && is_null($validToken)) {
 }
 
 
-
-
 $token = $_SESSION["token"];
 
 $fall = $_POST["fall"];
@@ -42,19 +41,20 @@ $winter = $_POST["winter"];
 $spring = $_POST["spring"];
 $summer = $_POST["summer"];
 $lastUpdate = date("Y-m-d h:i:s");
+$advisor = $_POST["advisor"];
 
 if (!empty($_POST)) {
     $select = "SELECT * FROM `adviseIt` WHERE token = '$token'";
     $result = mysqli_query($cnxn, $select);
 
     if (empty(mysqli_fetch_row($result))) {
-        $sql = "INSERT INTO adviseIt (token, fall, winter, spring, summer, lastUpdate)
-VALUES('$token','$fall','$winter','$spring','$summer','$lastUpdate')";
+        $sql = "INSERT INTO adviseIt (token, fall, winter, spring, summer, lastUpdate, advisor)
+VALUES('$token','$fall','$winter','$spring','$summer','$lastUpdate', '$advisor')";
 
         mysqli_query($cnxn, $sql);
     } else {
         $sql = "UPDATE adviseIt SET fall = '$fall', winter = '$winter', spring  = '$spring',
-                    summer= '$summer', lastUpdate = '$lastUpdate' WHERE token = '$token'";
+                    summer= '$summer', lastUpdate = '$lastUpdate', advisor = $advisor WHERE token = '$token'";
 
         mysqli_query($cnxn, $sql);
     }
@@ -66,82 +66,90 @@ VALUES('$token','$fall','$winter','$spring','$summer','$lastUpdate')";
 
 ?>
 
-<link rel="stylesheet" href="styles/planner.css">
-<div id="token">
-    <?php
-    print "<h1> Token: " . $_SESSION["token"]
-    ?>
-</div>
-<br>
+    <link rel="stylesheet" href="styles/planner.css">
+    <div id="token">
+        <?php
+        print "<h1> Token: " . $_SESSION["token"]
+        ?>
+    </div>
+    <br>
 
-<form action="#" method="post">
-    <div id="rows">
+    <form action="#" method="post">
+        <div id="rows">
 
-        <div class="quarter">
-        <label for="fall">Fall</label><br>
-        <textarea id="fall" name="fall" rows="5">
+            <div class="quarter">
+                <label for="fall">Fall</label><br>
+                <textarea id="fall" name="fall" rows="5">
         <?php echo $fall;
-        if(empty($_POST) || is_null($validToken))
-        {
+        if (empty($_POST) || is_null($validToken)) {
             echo $newFall;
         }
         ?>
     </textarea>
-        <br>
-    </div>
+                <br>
+            </div>
 
 
-    <div class="quarter">
-        <label for="winter">Winter</label><br>
-        <textarea id="winter" name="winter" rows="5">
+            <div class="quarter">
+                <label for="winter">Winter</label><br>
+                <textarea id="winter" name="winter" rows="5">
         <?php
         echo $winter;
-        if(empty($_POST) || is_null($validToken))
-        {
+        if (empty($_POST) || is_null($validToken)) {
             echo $newWinter;
         }
         ?>
     </textarea>
-        <br>
-    </div>
+                <br>
+            </div>
 
 
-    <div class="quarter">
-        <label for="spring">Spring</label><br>
-        <textarea id="spring" name="spring" rows="5">
+            <div class="quarter">
+                <label for="spring">Spring</label><br>
+                <textarea id="spring" name="spring" rows="5">
         <?php
         echo $spring;
-        if(empty($_POST) || is_null($validToken))
-        {
-          echo $newSpring;
+        if (empty($_POST) || is_null($validToken)) {
+            echo $newSpring;
         }
         ?>
     </textarea>
-        <br>
-    </div>
+                <br>
+            </div>
 
 
-    <div class="quarter">
-        <label for="summer">Summer</label><br>
-        <textarea id="summer" name="summer" rows="5">
+            <div class="quarter">
+                <label for="summer">Summer</label><br>
+                <textarea id="summer" name="summer" rows="5">
         <?php
         echo $summer;
-        if(empty($_POST) || is_null($validToken))
-        {
-          echo $newSummer;
+        if (empty($_POST) || is_null($validToken)) {
+            echo $newSummer;
         }
         ?>
    </textarea>
-        <br>
-    </div>
-    </div>
+                <br>
+            </div>
+        </div>
+        <div id="advisor2">
+            <label for="advisor">Advisor:</label><br>
+            <textarea id="advisor" name="advisor">
+                 <?php
+                 echo $advisor;
+                 if (empty($_POST) || is_null($validToken)) {
+                     echo $newAdvisor;
+                 }
+                 ?>
+        </textarea>
+        </div>
 
-    <div id="savePlan">
-    <button id="save" type="submit"> Save</button>
-    </div>
 
-    
-</form>
+        <div id="savePlan">
+            <button id="save" type="submit"> Save</button>
+        </div>
+
+
+    </form>
 
 <?php
 if (!empty($_POST)) {
